83969ae42a33185eb00d169d7186af77b36f4743
LoloLange <lololange08@gmail.com>
2024-02-08 19:15:01 -0300

    adapted .env to vercel

--- src/hooks/useWeatherData.js
+++ src/hooks/useWeatherData.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { useState } from 'react'
 
 const useWeatherData = () => {
@@ -7,7 +8,7 @@ const useWeatherData = () => {
 
     const fetchData = async (city) => {
         try {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=sp,es&appid=${import.meta.env.VITE_WEATHER_APP_API_KEY}&units=metric`)
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=sp,es&appid=${process.env.VITE_WEATHER_APP_API_KEY}&units=metric`)
             const data = await response.json();
             setWeatherData(data);
         } catch (e) {
